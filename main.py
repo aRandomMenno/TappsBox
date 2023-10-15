7d1860cda9161a2f6ed1734f7ddc5ebf122d1d6f
Menno <menno.van.zoelen@gmail.com>
2023-10-15 17:48:39 +0200

    Minor code polishing.
    - Fixed priority.
    - Made it run a little faster (I think).
    - Removed console feedback.

--- main.py
+++ main.py
@@ -1,73 +1,61 @@
 
-#? Importing necessary libraries
+#! Importing necessary libraries
 import pyautogui as pag
 import keyboard as kb
 import time
 import asyncio
 
-#* Initializing the cycles variable
 cycles = 0
 
-#* Defining the box function
+#! Defining the box function
 async def box():
-  #* Making cycles a global variable so it can be accessed outside the function
+  
   global cycles
-  
-  #* Locating the box, evo_box, and take_evo_box images on the screen
-  box = pag.locateOnScreen("C:\\Users\\menno\\OneDrive\\Coding\\Python\\Auto-Box\\img\\box.png", confidence=0.425)
-  evo_box = pag.locateOnScreen("C:\\Users\\menno\\OneDrive\\Coding\\Python\\Auto-Box\\img\\evo-box.png", confidence=0.6)
-  take_evo_box = pag.locateOnScreen("C:\\Users\\menno\\OneDrive\\Coding\\Python\\Auto-Box\\img\\take-btn.png", confidence=0.6)
-  
-  #* Incrementing cycles by 1
+  box = pag.locateOnScreen(r"C:\Users\menno\OneDrive\Coding\Python\Auto-Box\img\box.png", confidence=0.5)
+  evo_box = pag.locateOnScreen(r"C:\Users\menno\OneDrive\Coding\Python\Auto-Box\img\evo-box.png", confidence=0.6)
+  take_evo_box = pag.locateOnScreen(r"C:\Users\menno\OneDrive\Coding\Python\Auto-Box\img\take-btn.png", confidence=0.6)
   cycles += 1
-  
-  #* Checking if box, evo_box, or take_evo_box is found on the screen and performing actions accordingly
-  if box is not None:
-    print("INFO: Normal box.")
+
+  #! checking if any boxes are found or we need to take the evobox reward.
+  if take_evo_box is not None:
+    pag.moveTo(take_evo_box)
+    pag.leftClick()
+  elif box is not None:
     pag.moveTo(box)
     pag.leftClick()
   elif evo_box is not None:
-    print("INFO: Evolution box.")
     pag.moveTo(evo_box)
     pag.leftClick()
-  elif take_evo_box is not None:
-    print("INFO: 'take it' btn.")
-    pag.moveTo(take_evo_box)
-    pag.leftClick()
 
-#* Defining the berry function
+#! Defining the berry function
 async def berry():
-  #* Locating the blueBerry, redBerry, and yellowBerry images on the screen
-  blueBerry = pag.locateOnScreen("C:\\Users\\menno\\OneDrive\\Coding\\Python\\Auto-Box\\img\\berry-blue.png", confidence=0.5)
-  redBerry = pag.locateOnScreen("C:\\Users\\menno\\OneDrive\\Coding\\Python\\Auto-Box\\img\\berry-red.png", confidence=0.5)
-  yellowBerry = pag.locateOnScreen("C:\\Users\\menno\\OneDrive\\Coding\\Python\\Auto-Box\\img\\berry-yellow.png", confidence=0.5)
+  blueBerry = pag.locateOnScreen(r"C:\Users\menno\OneDrive\Coding\Python\Auto-Box\img\berry-blue.png", confidence=0.5)
+  redBerry = pag.locateOnScreen(r"C:\Users\menno\OneDrive\Coding\Python\Auto-Box\img\berry-red.png", confidence=0.5)
+  yellowBerry = pag.locateOnScreen(r"C:\Users\menno\OneDrive\Coding\Python\Auto-Box\img\berry-yellow.png", confidence=0.5)
   
-  #* Checking if blueBerry, redBerry, or yellowBerry is found on the screen and performing actions accordingly
+  #! Check for berries.
   if blueBerry is not None:
-    print("INFO: Blue berry.")
     pag.moveTo(blueBerry)
     pag.leftClick()
   elif redBerry is not None:
-    print("INFO: Red berry.")
     pag.moveTo(redBerry)
     pag.leftClick()
   elif yellowBerry is not None:
-    print("INFO: Yellow berry.")
     pag.moveTo(yellowBerry)
     pag.leftClick()
 
-#* Waiting for 3 seconds before starting the bot
+#! Waiting for 3 seconds before starting the bot
 print("START: Starting in 3 seconds.")
 time.sleep(3)
-print("START: Succesfully started 'bot'.")
+print("START: Succesfully started script.")
 
-#* Making sure you can stop the bot from running by pressing / holding the 'q' AND 's' at the same time a bit.
+#! Making sure you can stop the script from running by pressing / holding the 'q' AND 's' at the same time.
 while kb.is_pressed('q') == False:
-  #* Making sure you can pause the bot by pressing / holding the 'q' for a bit
-    while kb.is_pressed('q') == False:
+  #! Making sure you can pause the script by pressing / holding the 'q' for a bit
+    while kb.is_pressed('s') == False:
       asyncio.run(box())
-      #* Running the berry function every 5 cycles
       if cycles >= 5:
         asyncio.run(berry())
-        #* Resetting cycles to 0
         cycles = 0
+
+print("STOP: Stopped the script succesfully.")
